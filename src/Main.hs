5ff3e6f5f6cc6097b02ef38a39883d1aa5bfb35e
Rhys ! <rimmington@gmail.com>
2015-01-24 20:52:12 -0800

    try out doctest

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,14 @@
 -- | Main entry point to the application.
 module Main where
 
+-- |
+-- Increment a number.
+--
+-- >>> incr 2
+-- 3
+incr :: (Num a) => a -> a
+incr = (+ 1)
+
 -- | The main entry point.
 main :: IO ()
 main = do
